3d83f91fbd7f0366bea3e0d47f579dadf89e19ea
May Zuly <mayzuly.oo@outlook.com>
2024-08-05 23:26:57 +0700

    mirgrate

--- Program.cs
+++ Program.cs
@@ -38,14 +38,19 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+// Add services to the container.
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 }
 
 app.UseRouting();
